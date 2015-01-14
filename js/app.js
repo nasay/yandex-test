b17ea6bb6af79367b4420ae95be2c6468f7efad6
Natalia <nsay92@gmail.com>
2015-01-14 10:02:01 +0300

    rm unnecessery files, add comments

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ $(function () {
     App.prototype.start = function () {
         var self = this;
 
+        // Получение списка презентаций с сервера.
         $.getJSON('json/p-list.json', function (data) {
             self.list = new List(data.result);
 
